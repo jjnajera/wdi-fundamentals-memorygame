7d7653cc1d046983586bc06838c072ca33ff4183
Najera <xoy638@dca9047643e7.cof.ds.capitalone.com>
2017-07-19 16:00:25 -0400

    created objects for all cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+  {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.jpg'
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.jpg'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.jpg'
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.jpg'
+  }
+];
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -11,8 +32,10 @@ var checkForMatch = function(){
 };
 
 var flipCard = function(cardId){
-  cardsInPlay.push(cards[cardId]);
-  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 
   if(cardsInPlay.length === 2){
     checkForMatch();
