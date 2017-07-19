e49b30a6639610b5d22427735d3195044095685f
Najera <xoy638@dca9047643e7.cof.ds.capitalone.com>
2017-07-19 11:54:26 -0400

    created simple game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
+//Get first card and add it to cardsInPlay
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2){
+  if(cardOne === cardTwo){
+    alert("You found a match!");
+  }
+  else{
+    alert("Sorry, try again.");
+  }
+}
+
+console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardFour);
