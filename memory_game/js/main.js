f5e2efdf73ba29ea49012306f3a2dde34190068a
Najera <xoy638@dca9047643e7.cof.ds.capitalone.com>
2017-07-20 16:41:29 -0400

    implemented reset button, card shuffling, & score keeping

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,28 +21,46 @@ var cards = [
   }
 ];
 var cardsInPlay = [];
+var resetButton = document.getElementsByTagName('button')[0];
+var matchScore = 0;
+var nonMatchScore = 0;
 
 var checkForMatch = function(){
   if(cardsInPlay[0] === cardsInPlay[1]){
+    matchScore += 2;
+    var temp = document.getElementById('matchScore').textContent = "Match Score: "+matchScore;
     alert("You found a match!");
   }
   else{
+    nonMatchScore += 2;
+    var temp = document.getElementById('nonMatchScore').textContent = "Non-match Score: "+nonMatchScore;
     alert("Sorry, try again.");
   }
 };
 
 var flipCard = function(){
+  if(resetButton.getAttribute('disabled')){
+    resetButton.removeAttribute('disabled');
+    resetButton.style.backgroundColor= "#00FF00";
+    resetButton.style.color = "black";
+  }
+
   var cardId = this.getAttribute('data-id');
   this.setAttribute('src',cards[cardId].cardImage);
 
   cardsInPlay.push(cards[cardId].rank);
 
   if(cardsInPlay.length === 2){
-    checkForMatch();
+    setTimeout(checkForMatch,100);
   }
 };
 
 var createGameBoard = function(){
+
+  resetButton.addEventListener('click', resetGame);
+
+  cards.sort(function(){return 0.5-Math.random()});
+
   for(var i = 0;i < cards.length; i++){
     var newImage = document.createElement('img');
     newImage.setAttribute('src','images/back.png');
@@ -52,6 +70,24 @@ var createGameBoard = function(){
 
     document.getElementById('game-board').appendChild(newImage);
   }
+};
+
+var resetGame = function(){
+  resetButton.setAttribute('disabled',true);
+  resetButton.style.backgroundColor = "red";
+  resetButton.style.color = "white";
+
+  var parent = document.getElementById('game-board');
+  var images = document.getElementsByTagName('img');
+  for(var i = images.length-1; i >= 0; i--){
+    parent.removeChild(images[i]);
+  }
+
+  while(cardsInPlay.length != 0){
+    cardsInPlay.pop();
+  }
+
+  createGameBoard();
 };
 
 createGameBoard();
